99b473b8fd3241fea1eac66e3cf4d01af3aafec9
brodi77a <budialfianz@gmail.com>
2017-05-26 22:01:26 -0400

    completed

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,7 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
+for(i=0; i <=100; i++) {
+    console.log(i);
+}
 
 /*
 Exercise #2
@@ -17,3 +19,13 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var hashTag= '';
+
+for(i = 0; i < 7; i++) {
+    for(j = 0; j <= i; j++) {
+       hashTag+= '#';
+    }
+    console.log(hashTag);
+    var hashTag = '';
+}
